687175eb35d077700740d95f69318919eda6995c
Jax Harlan <tsmithtravis555@gmail.com>
2026-05-08 11:55:06 -0700

    gc

---
+++ providers/run.ps1
@@ -0,0 +1,15 @@
+param(
+    [string],
+    [string],
+    [int] = 512
+)
+
+ = "C:\llama.cpp\build\bin\Release\llama-cli.exe"
+
+&  
+    -m  
+    -p  
+    -n  
+    --temp 0.7 
+    --top-k 40 
+    --top-p 0.95
